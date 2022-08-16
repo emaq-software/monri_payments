afbfa3ca2126dd8a8b1ec4cb868c9683649779a5
anisalibegic <anis.alibegic@gmail.com>
2022-08-16 21:52:16 +0200

    Added BuildConfig

--- android/build/generated/source/buildConfig/release/com/monri/flutter/BuildConfig.java
+++ android/build/generated/source/buildConfig/release/com/monri/flutter/BuildConfig.java
@@ -4,9 +4,9 @@
 package com.monri.flutter;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = false;
   public static final String LIBRARY_PACKAGE_NAME = "com.monri.flutter";
-  public static final String BUILD_TYPE = "debug";
-  // Field from build type: debug
+  public static final String BUILD_TYPE = "release";
+  // Field from build type: release
   public static final String MONRI_FLUTTER_PLUGIN_VERSION = "1.0.0";
 }
