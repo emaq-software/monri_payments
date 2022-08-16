d383f0b5e7c21c25a37147e4a03243ebee7d3d2b
anisalibegic <anis.alibegic@gmail.com>
2022-08-16 21:54:53 +0200

    Minor changes

---
+++ android/build/generated/source/buildConfig/debug/com/monri/flutter/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.monri.flutter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.monri.flutter";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String MONRI_FLUTTER_PLUGIN_VERSION = "1.0.0";
+}
